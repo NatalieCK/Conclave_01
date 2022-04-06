06edf2791ccc488ebe888e03456a026698f7325f
NatalieCK <270053597@yoobeestudent.ac.nz>
2022-04-07 08:33:16 +1200

    file_upload

---
+++ front/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import Avatar from "./components/Avatar.vue";
+import LoadingAnimation from "./components/LoadingAnimation.vue";
+
+</script>
+
+<template>
+
+<router-link to="/">HOME_LINK</router-link>
+<br>
+<router-link to="/about">CONCLAVEINTRO_LINK</router-link>
+
+<Avatar />
+<router-view></router-view>
+<LoadingAnimation />
+
+</template>
+
+<style>
+
+@import './assets/base.css/';
+
+</style>
