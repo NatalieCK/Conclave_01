2a17c1366677dff1294033e1433fd53897a3c2c2
joedaviesio <joedaviesio@protonmail.com>
2022-04-11 17:40:55 +1200

    WritePostComponent

---
+++ front/src/components/WritePost.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+
+    <form class="inputs" action="#" method="GET"  id="">
+        
+        <fieldset class="fieldset">
+
+        <label for=""></label>
+        <input type="text" name="WritePost" placeholder="Write a post" class="input" required>
+
+      </fieldset>
+
+      </form>
+
+</div>
+</template>
+<style scoped>
+.fieldset {
+    border: solid white 1px;
+    border-right: none;
+    border-left: none;
+    height: 80px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+
+.input {
+    border: solid 1px white;
+    width: 322px;
+    height: 34px;
+    border-radius: 22.5px;
+    background-color: transparent;
+    opacity: 1;
+
+}
+::placeholder { 
+  color: white;
+  opacity: 1; 
+    font-family: 'inter';
+text-align: center;
+
+}
+
+</style>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -2,6 +2,7 @@
 import U_Avatar from '../components/U_Avatar.vue';
 import P_Avatar from '../components/P_Avatar.vue';
 import C_Avatar from '../components/C_Avatar.vue';
+import WritePost from '../components/WritePost.vue';
 </script>
 
 <template>
@@ -9,6 +10,7 @@ import C_Avatar from '../components/C_Avatar.vue';
 <U_Avatar/>
 <P_Avatar/>
 <C_Avatar/>
+<write-post/>
 
 <h1>Home</h1>
 </div>
