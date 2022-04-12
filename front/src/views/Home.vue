77c0a039a38106bec24cb57b82e5d3974010d8b1
joedaviesio <joedaviesio@protonmail.com>
2022-04-13 09:45:44 +1200

    Update Home.vue

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,9 +1,5 @@
 <script setup>
-import HomeNav from '../components/HomeNav.vue';
-import U_Avatar from '../components/U_Avatar.vue';
-import P_Avatar from '../components/P_Avatar.vue';
-import C_Avatar from '../components/C_Avatar.vue';
-import WritePost from '../components/WritePost.vue';
+
 
 
 
@@ -11,11 +7,7 @@ import WritePost from '../components/WritePost.vue';
 
 <template>
 <div>
-<HomeNav />
-<U_Avatar/>
-<P_Avatar/>
-<C_Avatar/>
-<WritePost/>
+
 
 
 </div>
